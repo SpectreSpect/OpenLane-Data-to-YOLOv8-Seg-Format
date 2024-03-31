1bd8bcc9b3c61ca2f20efe7f8af13728b768c204
spectre <spectrefromthefuture@gmail.com>
2024-03-31 23:04:52 +0400

    Update convert_to_yolo.py file

--- convert_to_yola.py
+++ convert_to_yola.py
@@ -1,6 +1,7 @@
 import json
 import os
 import shutil
+from timeit import default_timer as timer
 
 
 category = {
@@ -20,6 +21,16 @@ category = {
     20: 13,
     21: 14
     }
+
+
+def float_seconds_to_time_str(seconds, decimal_places_to_round_to):
+    if seconds < 60.0:
+        time = f"{round(seconds, decimal_places_to_round_to)} seconds"
+    elif seconds / 60.0 < 60.0:
+        time = f"{round(seconds / 60.0, decimal_places_to_round_to)} minutes"
+    else:
+        time = f"{round((seconds / 60.0) / 60.0, decimal_places_to_round_to)} hours"
+    return time
 
 
 def get_lane_line(json_file, line_idx) -> dict:
@@ -64,24 +75,36 @@ def convert_label_file_to_yolo(label_file_path, output_path):
         out.write(output_string)
 
 
-def convert_segment_to_yolo(segment_path, output_path):
+def convert_segment_to_yolo(segment_path, output_path, max_items=-1, avalible_file_names=None) -> list:
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     
+    converted_file_names = []
+    
+    idx = 0
     for full_filename in os.listdir(segment_path):
         filename = full_filename.split(".", 1)[0]
+        
+        if avalible_file_names is not None:
+            if filename not in avalible_file_names:
+                continue
         
         original_file_path = os.path.join(segment_path, full_filename)
         yolo_file_path = os.path.join(output_path, filename + ".txt")
         
         if os.path.isfile(original_file_path):
+            if max_items >= 0:
+                if idx >= max_items:
+                    break
             convert_label_file_to_yolo(original_file_path, yolo_file_path)
+            converted_file_names.append(filename)
+            idx += 1
+    return converted_file_names
 
 
 def convert_labels_to_yolo(labels_path, output_path):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
-    
     for item_name in os.listdir(labels_path):
         full_path = os.path.join(labels_path, item_name)
         output_segment_path = os.path.join(output_path, item_name)
@@ -90,7 +113,54 @@ def convert_labels_to_yolo(labels_path, output_path):
             convert_segment_to_yolo(full_path, output_segment_path)
 
 
-def convert_dataset_to_yolo(dataset_path, output_path):
+def copy_images(segment_path, images_target_path, max_items=-1):
+    if os.path.isdir(segment_path):
+        idx = 0
+        for image_name in os.listdir(segment_path):
+            source_path = os.path.join(segment_path, image_name)
+            if os.path.isfile(source_path):
+                if max_items >= 0:
+                    if idx >= max_items:
+                        break
+                shutil.copy(source_path, images_target_path)
+                idx += 1
+
+
+def get_file_names(path: str, remove_extension=False) -> list:
+    files = []
+    if os.path.isdir(path):
+        for file_name in os.listdir(path):
+            
+            
+            file_path = os.path.join(path, file_name)
+            if os.path.isfile(file_path):
+                if remove_extension:
+                    file_name = file_name.split('.', 1)[0]
+                files.append(file_name)
+    return files
+
+
+def copy_files(source_path, target_path, max_items=-1, avalible_file_names=None):
+    
+    idx = 0
+    for file_name in os.listdir(source_path):
+        
+        if avalible_file_names is not None:
+            no_ext_file_name = file_name.split('.', 1)[0]
+            if no_ext_file_name not in avalible_file_names:
+                continue
+        
+        file_path = os.path.join(source_path, file_name)
+        if os.path.isfile(file_path):
+            if max_items >= 0:
+                if idx >= max_items:
+                    break
+            shutil.copy(file_path, target_path)
+            idx += 1
+    
+
+
+def convert_dataset_to_yolo(dataset_path, output_path, max_items_count=-1):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
         
@@ -100,27 +170,59 @@ def convert_dataset_to_yolo(dataset_path, output_path):
     if not os.path.exists(output_path + "/images/train/"):
         os.makedirs(output_path + "/images/train/")
     
-    target_path = os.path.join(output_path, 'labels', 'train')
     labels_path = dataset_path + "/labels/"
-    for item_name in os.listdir(labels_path):
-        source_path = os.path.join(labels_path, item_name)
-        
-        
-        if os.path.isdir(source_path):
-            convert_segment_to_yolo(source_path, target_path)
-    
-    
-    target_path = os.path.join(output_path, 'images', 'train')
     images_path = dataset_path + "/images/"
-    for item_name in os.listdir(images_path):
-        segment_path = os.path.join(images_path, item_name)
+    
+    image_segments = os.listdir(images_path)
+    label_segments = os.listdir(labels_path)
+    available_segment_names = set(image_segments) & set(label_segments)
+    
+    segments_count = len(available_segment_names)
+    converted_segments_count = 0
+    converted_items_count = 0
+    left_items_count = max_items_count
+    
+    labels_target_path = os.path.join(output_path, 'labels', 'train')
+    images_target_path = os.path.join(output_path, 'images', 'train')
+
+    start = timer()
+    for idx, item_name in enumerate(available_segment_names):
+        if left_items_count <= 0:
+            break
         
-        if os.path.isdir(segment_path):
-            for image_name in os.listdir(segment_path):
-                source_path = os.path.join(segment_path, image_name)
-                if os.path.isfile(source_path):
-                    shutil.copy(source_path, target_path)
+        # if max_segments_count >= 0:
+        #     if idx >= max_segments_count:
+        #         break
+        
+        label_segment_path = os.path.join(labels_path, item_name)
+        image_segment_path = os.path.join(images_path, item_name)
+        
+        if os.path.isdir(label_segment_path) and os.path.isdir(image_segment_path):
+            label_names = get_file_names(label_segment_path, True)
+            image_names = get_file_names(image_segment_path, True)
+            
+            available_file_names = set(label_names) & set(image_names)
+            
+            if len(available_file_names) > 0:
                 
+                
+                converted_file_names = convert_segment_to_yolo(label_segment_path, labels_target_path, 
+                                                               avalible_file_names=available_file_names, max_items=left_items_count)
+                copy_files(image_segment_path, images_target_path, avalible_file_names=converted_file_names)
+                converted_segments_count += 1
+                converted_items_count += len(converted_file_names)
+                
+                left_items_count = max_items_count - converted_items_count
+                
+                end = timer()
+                
+                elapsed_time = end - start
+                eta = (elapsed_time / converted_items_count) * left_items_count
+                
+                print(f"segments: {converted_segments_count}/{segments_count}   items: {converted_items_count}/{max_items_count}    "
+                      f"eta: {float_seconds_to_time_str(eta, 2)}    "
+                      f"elapsed: {float_seconds_to_time_str(elapsed_time, 2)}")
+
 
 if __name__ == "__main__":
-    convert_dataset_to_yolo("data/openlane", "data/yolov8")
+    convert_dataset_to_yolo("data/openlane", "/media/spectre/74DCDE42DCDDFE74/Torrent/yolov8_2", max_items_count=10000)
